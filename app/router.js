d71b3b8679656b61c63782a653800dec9e38ca74
flipperflipper <caylaphil@gmail.com>
2021-12-12 14:54:20 +0100

    fourth commit

--- app/router.js
+++ app/router.js
@@ -10,5 +10,28 @@ router.get("/",(req,res)=>{
     res.render("index")
 });
 
+router
+
+router.get("/game/:nameGame", (req, res)=>{
+
+    const gameName = req.params.nameGame;
+
+    const game = res.locals.games.find(myGame => myGame.name === gameName);
+    console.log("game", game);
+
+    if (game) {
+
+        const dataForView = {
+            jsFile: game.jsFile,
+            cssFile: game.cssFile,
+        }
+
+        res.render(game.name, dataForView);
+    }
+    else {
+        next();
+    }
+});
+
 // Exporting router 
 module.exports = router;
--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const express = require("express");
 const server = express();
 
 // Requesting router
-
 const router = require("./app/router");
 
 // Indicating what model we're using
@@ -15,6 +14,16 @@ server.set('views', './app/views');
 
 // Indicating the static public folder 
 server.use(express.static('public'));
+
+const games = require("./games.json");
+
+server.use((req,res,next)=>{
+    // j'enregistre dans les locals mes jeux
+    res.locals.games = games;
+    console.log("middleware dans l'index");
+
+    next();
+});
 
 // Requesting router
 server.use(router);
